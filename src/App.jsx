67225c2546de9fe3e39504a6b05b51364f5b2acd
Enyel Sequeira <enyelsequeira@hotmail.com>
2021-11-22 17:54:52 +0800

    adding some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
+import { motion } from "framer-motion";
 import "./App.css";
 
 function App() {
   const wave = () => {};
   return (
-    <div className="container mx-auto h-ful border-2 border-black">
-      <h1 class="text-center  my-5 text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-red-600">
+    <div className="container mx-auto min-h-[100vh] bg-gradient-to-r from-purple-100 to-red-100 rounded-2xl">
+      <h1 class="text-center my-5 py-12 text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-red-600">
         Send a wave and chill 👋🏼
       </h1>
 
-      <div className="bg-gradient-to-r from-pink-600 to-blue-400">
-        <div className=" flex flex-col items-center space-y-5 ">
-          <p className="text-2xl">
+      <div className="bg-gradient-to-r  from-purple-200 to-red-200 rounded-2xl mt-11 py-10 ">
+        <div className=" flex flex-col items-center space-y-5">
+          <p className="text-3xl leading-loose tracking-wide my-2 p-7 w-2/3 text-center">
             My name is Enyel, I am currently working on learning about web3 and
             solidity ecosystem.
           </p>
 
-          <p>Lets connect and build something together.</p>
+          <p className="text-2xl tracking-wide p-6">
+            Lets connect and build something together.
+          </p>
         </div>
 
-        <button
-          className="w-[fit-content] flex  px-3 py-2 border-2 bg-gradient-to-r from-purple-300  to-red-300 font-extrabold tracking-wider mx-auto"
+        <motion.button
+          className="w-[fit-content] flex  px-3 py-2 border-2 bg-gradient-to-r from-purple-300  to-red-300 font-extrabold tracking-wider mx-auto rounded-t-lg rounded-r-lg"
           onClick={wave}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
         >
           Wave at Me
-        </button>
+        </motion.button>
       </div>
     </div>
   );
---
+++ src/components/animated-svg.jsx
@@ -0,0 +1,9 @@
+import { motion } from "framer-motion";
+
+const transition = { duration: 4, yoyo: Infinity, ease: "easeInOut" };
+
+const AnimatedSvg = () => {
+  return <div className="container bg-red-200"></div>;
+};
+
+export default AnimatedSvg;
